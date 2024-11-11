3813274b5ef28f1d683696f765226ee72a9fc066
nkruger <nicolas.nkruger@gmail.com>
2024-11-11 14:27:11 -0300

    feat: :sparkles: baianor

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,18 @@
-<?php
-echo "Hello, World!";
-?>
+
+<p>
+    <?="tag curta "?>
+</p>
+
+<P>
+    <?php
+        echo "Hello, World!";
+        $baiano_vivo = rand(0, 1) == 1;
+    ?>
+</P>
+<p> baiano vai estar vivo ? <p>
+<?php if ($baiano_vivo == true): ?>
+    <p>sem caixão</p>
+<?php else: ?>
+    <p>caixão fechado</p>
+<?php endif; ?>
+
